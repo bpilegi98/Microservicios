71797047855d3ade1ff8e005e83fdb995b760ca6
Bianca Pilegi <bpilegi@mobydigital.com>
2021-02-24 16:50:43 -0300

    Servicio Items
    Commit inicial del servicio items funcionando todo OK

---
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
@@ -0,0 +1,40 @@
+package com.formacionbdi.springboot.app.item.models;
+
+import java.util.Date;
+
+public class Producto {
+	
+	private Long id;
+	private String name;
+	private Double precio;
+	private Date createAt;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	
+	
+}
