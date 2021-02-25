b96eadc8756ec90c975fe2df1f7db2ea070ae44e
Bianca Pilegi <bpilegi@mobydigital.com>
2021-02-25 12:22:29 -0300

    Tolerancio de fallos y latencia con Hystrix funcionando OK

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -26,9 +28,25 @@ public class ItemController {
 	}
 	
 	
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable int cantidad)
 	{
 		return itemService.findById(id, cantidad);
+	}
+	
+	
+	public Item metodoAlternativo(Long id, int cantidad)
+	{
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		
+		return item;
 	}
 }
