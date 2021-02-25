882f1d21f0426c030b40c2ee960cdba284e6f9ed
Bianca Pilegi <bpilegi@mobydigital.com>
2021-02-25 08:52:17 -0300

    Modificando Service de Item agregando Interfaz + Implementación

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/service/ItemService.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/service/ItemService.java
@@ -1,36 +1,11 @@
 package com.formacionbdi.springboot.app.item.service;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.formacionbdi.springboot.app.item.models.Item;
-import com.formacionbdi.springboot.app.item.models.Producto;
 
-@Service
-public class ItemService {
-	
-	@Autowired
-	private RestTemplate clienteRest;
-	
-	public List<Item> findAll()
-	{
-		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:8001/listar", Producto[].class));
-		return productos.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
-	}
-	
-	public Item findById(Long id, int cantidad)
-	{
-		Map<String, String> pathVariables = new HashMap();
-		pathVariables.put("id", id.toString());
-		Producto producto = clienteRest.getForObject("http://localhost:8001/ver/{id}", Producto.class, pathVariables);
-		return new Item(producto, cantidad);
-	}
+public interface ItemService {
 
+	public List<Item> findAll();
+	public Item findById(Long id, Integer cantidad);
 }
