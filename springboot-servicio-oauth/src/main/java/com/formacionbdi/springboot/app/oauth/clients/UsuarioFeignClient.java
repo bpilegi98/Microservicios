6022bd6c7489d94475b2b30ebee8f49e1913391f
Bianca Pilegi <bpilegi@mobydigital.com>
2021-03-01 12:07:39 -0300

    Implementando servicio UserDetailsService con Feign Client

---
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/clients/UsuarioFeignClient.java
@@ -0,0 +1,16 @@
+package com.formacionbdi.springboot.app.oauth.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.commons.models.entity.Usuario;
+
+@FeignClient(name="servicio-usuarios")
+public interface UsuarioFeignClient {
+
+	@GetMapping("/usuarios/search/buscar-username")
+	public Usuario findByUsename(@RequestParam String username);
+	
+	
+}
