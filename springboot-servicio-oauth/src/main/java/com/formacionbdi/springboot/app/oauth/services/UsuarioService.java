6001e7b52b28c6b2b6b3b833fb88f450e6ddd2f1
Bianca Pilegi <bpilegi@mobydigital.com>
2021-03-01 12:29:21 -0300

    Añadiendo config para servidor de autorización

--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -12,10 +12,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.commons.models.entity.Usuario;
 import com.formacionbdi.springboot.app.oauth.clients.UsuarioFeignClient;
 
+@Service
 public class UsuarioService implements UserDetailsService{
 	
 	private Logger log = LoggerFactory.getLogger(UsuarioService.class);
